b2d8ac004da0adf721ce13627e850587a91f80e9
nikitashrestha <nikshres522@gmail.com>
2020-04-14 15:02:59 +0545

    Generate word cloud

--- visualize.py
+++ visualize.py
@@ -1,4 +1,4 @@
-from wordcloud import WordCloud, STOPWORDS
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
 import matplotlib.pyplot as plt
 import pandas as pd
 
@@ -7,36 +7,20 @@ Loading Datasets
 """
 df = pd.read_csv("amazon-fine-food-reviews/Reviews.csv")
 
-comment_words = ' '
-stopwords = set(STOPWORDS)
 
-# iterate through the csv file
-for val in df.Text:
+# Start with one review:
+text = df.Text
 
-    # typecaste each val to string
-    val = str(val)
+x = ""
+for t in text:
+    x = x + t
 
-    # split the value
-    tokens = val.split()
+# Create and generate a word cloud image:
+wordcloud = WordCloud(max_font_size=50, max_words=1000,
+                      width=1350, height=760).generate(x)
+wordcloud.to_file("first_review.png")
 
-    # Converts each token into lowercase
-    for i in range(len(tokens)):
-        tokens[i] = tokens[i].lower()
-
-    for words in tokens:
-        comment_words = comment_words + words + ' '
-
-
-wordcloud = WordCloud(width=1280, height=720,
-                      background_color='white',
-                      stopwords=stopwords,
-                      min_font_size=10).generate(comment_words)
-
-# plot the WordCloud image
-plt.figure(figsize=(8, 8), facecolor=None)
-plt.imshow(wordcloud)
+# Display the generated image:
+plt.imshow(wordcloud, interpolation='bilinear')
 plt.axis("off")
-plt.tight_layout(pad=0)
-
 plt.show()
-plt.savefig("plot.png")
